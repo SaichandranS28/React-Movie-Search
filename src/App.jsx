a681c002be233349afd19a13c1306539ec092d0b
Saichandran28 <s.saichandran2002@gmail.com>
2025-03-07 21:17:11 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <div className="mt-32">
+        <h1 className='text-center'>Hello</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
